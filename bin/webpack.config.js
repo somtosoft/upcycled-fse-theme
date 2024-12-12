5707cedc9e9916c7510dd23a56df970c69d251f9
somteacodes <somteacodes@gmail.com>
2024-12-12 01:38:43 +0000

    Refactor webpack configuration for consistent quotation style and improved readability

--- bin/webpack.config.js
+++ bin/webpack.config.js
@@ -1,9 +1,9 @@
-const defaultConfig = require("@wordpress/scripts/config/webpack.config");
-const RemovePlugin = require("remove-files-webpack-plugin");
-const path = require("path");
-const { getEntries } = require("./get-entries");
+const defaultConfig = require('@wordpress/scripts/config/webpack.config');
+const RemovePlugin = require('remove-files-webpack-plugin');
+const path = require('path');
+const { getEntries } = require('./get-entries');
 // styleOutputFolder should be relative to the root of the theme with no leading or trailing slashes
-const styleOutputFolder = "css";
+const styleOutputFolder = 'css';
 /**
  * Custom Webpack Configuration
  *
@@ -15,15 +15,15 @@ var config = {
 	...defaultConfig,
 	entry: {
 		...defaultConfig.entry(),
-		...getEntries({ root: "src/js", include: "*.js", outputFolder: "js" }),
+		...getEntries({ root: 'src/js', include: '*.js', outputFolder: 'js' }),
 		...getEntries({
-			root: "src/scss",
-			include: "*.scss",
+			root: 'src/scss',
+			include: '*.scss',
 			outputFolder: styleOutputFolder,
 		}),
 		...getEntries({
-			root: "src/scss/blocks",
-			include: "**/*.scss",
+			root: 'src/scss/blocks',
+			include: '**/*.scss',
 			outputFolder: styleOutputFolder,
 			blockDir: true,
 		}),
@@ -31,8 +31,8 @@ var config = {
 	output: {
 		...defaultConfig.output,
 		// change the output path for blocks to the blocks/ folder
-		path: path.resolve(process.cwd(), "blocks"),
-		assetModuleFilename: "../src/scss/utils/[name].scss",
+		path: path.resolve(process.cwd(), 'blocks'),
+		assetModuleFilename: '../src/scss/utils/[name].scss',
 	},
 	plugins: [
 		new RemovePlugin({
@@ -47,41 +47,50 @@ var config = {
 					{
 						folder: styleOutputFolder,
 						method: (absoluteItemPath) => {
-							return new RegExp(/\.js/, "m").test(absoluteItemPath);
+							return new RegExp(/\.js/, 'm').test(
+								absoluteItemPath
+							);
 						},
 					},
 					{
 						folder: styleOutputFolder,
 						method: (absoluteItemPath) => {
-							return new RegExp(/\.php$/, "m").test(absoluteItemPath);
-						},
-					},
-					{
-						folder: "./src/scss/utils",
-						method: (absoluteItemPath) => {
-							return new RegExp(/\.php$/, "m").test(absoluteItemPath);
-						},
-					},
-					{
-						folder: "./src/scss/utils",
-						method: (absoluteItemPath) => {
-							return new RegExp(/(\.js)(\.map)*$/, "m").test(absoluteItemPath);
-						},
-					},
-					{
-						folder: ".",
-						method: (absoluteItemPath) => {
-							return new RegExp(/(json\.js)(\.map)*$/, "m").test(
+							return new RegExp(/\.php$/, 'm').test(
 								absoluteItemPath
 							);
 						},
 					},
 					{
-						folder: ".",
+						folder: './src/scss/utils',
 						method: (absoluteItemPath) => {
-							return new RegExp(/theme\.json\.asset\.php$/, "m").test(
+							return new RegExp(/\.php$/, 'm').test(
 								absoluteItemPath
 							);
+						},
+					},
+					{
+						folder: './src/scss/utils',
+						method: (absoluteItemPath) => {
+							return new RegExp(/(\.js)(\.map)*$/, 'm').test(
+								absoluteItemPath
+							);
+						},
+					},
+					{
+						folder: '.',
+						method: (absoluteItemPath) => {
+							return new RegExp(/(json\.js)(\.map)*$/, 'm').test(
+								absoluteItemPath
+							);
+						},
+					},
+					{
+						folder: '.',
+						method: (absoluteItemPath) => {
+							return new RegExp(
+								/theme\.json\.asset\.php$/,
+								'm'
+							).test(absoluteItemPath);
 						},
 					},
 				],
@@ -90,6 +99,10 @@ var config = {
 		...defaultConfig.plugins,
 	],
 	cache: false,
+	// externals: {
+	// 	react: 'React',
+	// 	'react-dom': 'ReactDOM',
+	// },
 };
 
 // Return Configuration

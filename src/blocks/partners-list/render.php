7130ccb1927545bbdfcf6dae6e3c2347bdcfc672
somteacodes <somteacodes@gmail.com>
2024-12-12 01:39:21 +0000

    Add Partners List block with editor and front-end support

---
+++ src/blocks/partners-list/render.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
+ */
+?>
+<div class="flex  items-center justify-center gap-4 w-full">
+	<?php
+	$args =
+		array(
+			'post_type' => 'partner',
+
+		);
+	$partner = new WP_Query($args);
+
+
+	while ($partner->have_posts()) {
+		$partner->the_post();
+		$image = get_field('logo');
+
+	?>
+
+		<a class="hover:-translate-y-2 transition-transform duration-300 ease-in-out cursor-pointer max-w-xs"
+			href="<?php the_field('website'); ?>"
+			target="_blank"
+			rel="noopener noreferrer">
+
+
+
+			<img src="<?php echo $image['url']; ?>" alt="">
+		</a>
+
+
+
+
+<?php
+	}
+?>
+</div>

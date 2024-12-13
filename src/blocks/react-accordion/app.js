877599bf7535e0362556c4832dfc2dffd806f3f8
somteacodes <somteacodes@gmail.com>
2024-12-13 04:14:38 +0000

    Add form handling with validation to React Accordion block

--- src/blocks/react-accordion/app.js
+++ src/blocks/react-accordion/app.js
@@ -1,8 +1,25 @@
 // import useState from wordpress/element
-import { useState } from "@wordpress/element";
-
+import { useState } from '@wordpress/element';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+const schema = yup
+	.object({
+		firstName: yup.string().required(),
+		age: yup.number().positive().integer().required(),
+	})
+	.required();
 function App() {
 	const [isOpen, setIsOpen] = useState(false);
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: yupResolver(schema),
+	});
+	const onSubmit = (data) => console.log(data);
 
 	// an update function
 	function toggleButton() {
@@ -18,6 +35,16 @@ function App() {
 		<>
 			<button onClick={toggleButton}>Hello</button>
 			{isOpen && <p>World</p>}
+
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<input {...register('firstName')} />
+				<p>{errors.firstName?.message}</p>
+
+				<input {...register('age')} />
+				<p>{errors.age?.message}</p>
+
+				<input type="submit" />
+			</form>
 		</>
 	);
 }
